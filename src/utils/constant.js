2672bc9ffead98ad4918445abccf3319531e0969
ppobear <ppobear@qq.com>
2018-06-12 14:06:19 +0800

    [Feature] :首页：获取个人信息、日历、每日食物列表、当日早中晚餐分配 [Requirement] [Changes]:

---
+++ src/utils/constant.js
@@ -0,0 +1,45 @@
+/**
+ * 用户code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO = "userSpecialInfo";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO = "userInfo";
+
+/**
+ * 系统信息
+ * @type {String}
+ */
+export const SYSTEM_INFO = "systemInfo";
+
+
+export const ADDRESS_ID = "addressId";
+
+export const SEL_CLASS_CODE = "selClassCode";
+
+/**
+ * 容器信息
+ */
+export const CONTAINER_TYPE_VEG = "veg";
+
+export const CONTAINER_TYPE_FRUITS = "fruits";
+
+export const CONTAINER_TYPE_PORTEINS = "proteins";
+
+export const CONTAINER_TYPE_CARBS = "carbs";
+
+export const CONTAINER_TYPE_FAT = "fat";
+
+export const CONTAINER_TYPE_NUTS = "nuts";
+
+export const CONTAINER_VEG_COLOR = '#2DCC70';
+export const CONTAINER_FRUITS_COLOR = '#8870AC';
+export const CONTAINER_PROTEINS_COLOR = '#E74B3E';
+export const CONTAINER_CARBS_COLOR = '#FDD301';
+export const CONTANIER_FAT_COLOR = '#35B9E9';
+export const CONTAINER_NUTS_COLOR = '#F19B14';
+export const CONTAINER_DEFAULT_COLOR = '#EDEDED';
